6655b9cd735ca609649a1aa4df31a0ab7f81691c
Emin Üzümlüoğlu <euzumluoglu@yahoo.com.tr>
2017-05-23 22:26:08 +0300

    initial commit

---
+++ src/main/java/com/shopinle/config/Application.java
@@ -0,0 +1,32 @@
+package com.shopinle.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+}
